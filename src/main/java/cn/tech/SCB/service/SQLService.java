9762acf6b195475154bc941d0f5a5288a74ad503
shaun <17803821620@163.com>
2026-06-18 17:30:47 +0700

    initial project

--- src/main/java/cn/tech/SCB/service/SQLService.java
+++ src/main/java/cn/tech/SCB/service/SQLService.java
@@ -1,4 +1,19 @@
 package cn.tech.SCB.service;
 
+import java.util.List;
+
 public class SQLService {
+    /*
+    * 输入参数 : SQL所在的文件
+    * */
+    List<String> listTotalTableWithSqlFile(String sqlFile){
+
+    }
+    /*
+     * 输入参数1 : NameSql文件; 输入参数2 : nameSqlID
+     * */
+    List<String> listTotalTableWithNameSqlFile(String nameSqlFile, String NameSqlID){
+
+    }
+
 }
