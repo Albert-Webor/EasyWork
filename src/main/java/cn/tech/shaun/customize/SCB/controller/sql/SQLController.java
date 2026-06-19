0bba38b8d4668394e4337909d9fa5b91408c422f
shaun <17803821620@163.com>
2026-06-19 13:09:16 +0700

    目前位置: 接口AP001已经能够成功被前端调用并返回

--- src/main/java/cn/tech/shaun/customize/SCB/controller/sql/SQLController.java
+++ src/main/java/cn/tech/shaun/customize/SCB/controller/sql/SQLController.java
@@ -1,4 +1,23 @@
 package cn.tech.shaun.customize.SCB.controller.sql;
 
+import cn.tech.shaun.customize.SCB.dto.SQLAnalysisdto;
+import cn.tech.shaun.customize.SCB.service.SQLService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/sql")
 public class SQLController {
+    private final SQLService sqlService;
+
+    @PostMapping("/AP001")
+    public List<SQLAnalysisdto> AP001(@RequestBody String sql) {
+        return sqlService.lsSqlAnalysisByInputSqlString(sql);
+    }
 }
